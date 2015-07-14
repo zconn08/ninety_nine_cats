1f39cd18553c3162e5f41c25b2c1fb54dadcda9f
App Academy Student <student@appacademy.io>
2015-07-14 12:01:46 -0700

    test cat creation form

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,4 +8,24 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :show
   end
+
+  def new
+    render :new
+  end
+
+  def create
+    @cat = Cat.create(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat.id)
+    else
+      render :new
+      # not good for end user, would rather show errors and retry form submission
+    end
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:name, :sex, :color, :birth_date, :description)
+  end
 end
